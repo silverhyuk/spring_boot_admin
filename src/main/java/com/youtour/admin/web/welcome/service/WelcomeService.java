fe83b93c010f8809875db13bf6c7782ab84b261c
silverhyuk <tldn233@gmail.com>
2018-09-15 20:48:33 +0900

    transactional service 예제 추가

--- src/main/java/com/youtour/admin/web/welcome/service/WelcomeService.java
+++ src/main/java/com/youtour/admin/web/welcome/service/WelcomeService.java
@@ -2,14 +2,20 @@ package com.youtour.admin.web.welcome.service;
 
 import com.youtour.admin.web.welcome.dao.WelcomeMapper;
 import com.youtour.admin.web.welcome.vo.WelcomeVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class WelcomeService {
+
+    private static final Logger log = LoggerFactory.getLogger(WelcomeService.class);
+
     @Autowired
     private WelcomeMapper welcomeMapper;
 
@@ -19,19 +25,31 @@ public class WelcomeService {
         List<WelcomeVO> WelcomeList = welcomeMapper.selectWelcomeList(welcomeVO);
 
         for(WelcomeVO vo : WelcomeList) {
-            System.out.println("name : " + vo.getName() +  " / team : " + vo.getTeam());
+            log.debug("name : " + vo.getName() +  " / team : " + vo.getTeam());
         }
         return WelcomeList;
     }
 
     @Async("fooExecutor")
-    public String multiThreadTest() throws  Exception {
+    public List<WelcomeVO>  multiThreadTest() throws  Exception {
+
+        WelcomeVO welcomeVO = new WelcomeVO();
+        List<WelcomeVO> WelcomeList = welcomeMapper.selectWelcomeList(welcomeVO);
+
+        for(WelcomeVO vo : WelcomeList) {
+            log.debug("name : " + vo.getName() +  " / team : " + vo.getTeam());
+        }
+        return WelcomeList;
+    }
+
+    @Transactional
+    public String transactionalTest() throws  Exception {
         String ret = "TEST";
         WelcomeVO welcomeVO = new WelcomeVO();
         List<WelcomeVO> WelcomeList = welcomeMapper.selectWelcomeList(welcomeVO);
 
         for(WelcomeVO vo : WelcomeList) {
-            System.out.println("name : " + vo.getName() +  " / team : " + vo.getTeam());
+            log.debug("name : " + vo.getName() +  " / team : " + vo.getTeam());
         }
         return ret;
     }
